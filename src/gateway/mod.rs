1c8e932f371ca1663516948c4b98caba2d807453
Liam Kinne <liam@kinne.io>
2024-05-19 04:51:29 +1000

    move status command to module

--- src/gateway/mod.rs
+++ src/gateway/mod.rs
@@ -1,3 +1,5 @@
+mod status;
+
 use anyhow::Error;
 use clap::{Parser, Subcommand};
 use std::{
@@ -47,60 +49,12 @@ impl Cmd {
         let ctx = connect(socket_addr).await?;
 
         match self.subcommand {
-            Commands::Status => status(output, ctx).await,
+            Commands::Status => status::command(output, ctx).await,
             Commands::Update(options) => update(output, options, self.ip).await,
             Commands::Restart => restart(output).await,
             Commands::Reset => reset(output).await,
         }
     }
-}
-
-#[derive(Debug)]
-#[allow(unused)]
-enum ModbusSlave {
-    System = 1,
-    Network = 2,
-    Canbus = 3,
-}
-
-impl From<ModbusSlave> for Slave {
-    fn from(value: ModbusSlave) -> Self {
-        Slave(value as u8)
-    }
-}
-
-async fn status(
-    mut output: impl std::io::Write,
-    mut ctx: ModbusContext,
-) -> anyhow::Result<()> {
-    let start = Instant::now();
-
-    ctx.set_slave(ModbusSlave::System.into());
-
-    let hardware_version = ctx.read_holding_registers(1, 3).await?;
-    writeln!(
-        output,
-        "Hardware version: {}.{}.{}",
-        hardware_version[0], hardware_version[1], hardware_version[2]
-    )?;
-
-    let firmware_version = ctx.read_holding_registers(4, 3).await?;
-    writeln!(
-        output,
-        "Firmware version: {}.{}.{}",
-        firmware_version[0], firmware_version[1], firmware_version[2]
-    )?;
-
-    let uptime = u16_words_to_u32(&ctx.read_holding_registers(7, 2).await?)?;
-    writeln!(output, "Uptime: {} seconds", uptime)?;
-
-    writeln!(
-        output,
-        "Got status in {} seconds.",
-        start.elapsed().as_secs_f64()
-    )?;
-
-    Ok(())
 }
 
 #[derive(Parser)]
@@ -205,16 +159,4 @@ async fn restart(mut output: impl std::io::Write) -> anyhow::Result<()> {
 #[allow(unused_variables, unused_mut)]
 async fn reset(mut output: impl std::io::Write) -> anyhow::Result<()> {
     todo!();
-}
-
-fn u16_words_to_u32(words: &[u16]) -> anyhow::Result<u32> {
-    // construct a u32 from u16's observing network byte order.
-    let uptime: Vec<u8> = words
-        .iter()
-        .flat_map(|&word| vec![word as u8, (word >> 8) as u8])
-        .collect();
-
-    Ok(u32::from_be_bytes([
-        uptime[0], uptime[1], uptime[2], uptime[3],
-    ]))
 }
